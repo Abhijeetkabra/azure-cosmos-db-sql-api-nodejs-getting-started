54df6506a21bba0716374c081c06b53296e8bd46
AKABRA <akabra@worldbankgroup.org>
2023-04-25 17:24:07 +0530

    .

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // @ts-check
 
 const config = {
-  endpoint: "<Your Azure Cosmos account URI>",
-  key: "<Your Azure Cosmos account key>",
+  endpoint: "https://itsoc-test-engineering.documents.azure.com:443/",
+  key: "aMoMOUJI2udUJHhqPXpUpa5lyn1Lsv700eC7uuONto6KwYubQYLlWFcRAlhUcXTiAfTSUNmgmLNmACDbxFNmzQ==",
   databaseId: "Tasks",
   containerId: "Items",
   partitionKey: { kind: "Hash", paths: ["/category"] }
